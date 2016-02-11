f300450e706b04a619e1a995a340ad980d8a74ba
Yifei Li <lee891031@gmail.com>
2016-02-11 14:25:24 -0800

    Simple program split file into pieces

---
+++ FileSplit/FileSplit/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string InputFileName = args[0];
+            string Outputfolder = args[1];
+            int OutputCount = int.Parse(args[2]);
+            StreamReader reader = new StreamReader(InputFileName);
+
+
+            //Count number of lines
+            Console.WriteLine("Compute Lines..");
+            int count = 0;
+            while (!reader.EndOfStream)
+            {
+                reader.ReadLine();
+                count++;
+            }
+
+            Console.WriteLine("Total lines: " + count);
+            int chunkSize = count / OutputCount;
+            StreamReader reader2 = new StreamReader(InputFileName);
+
+            for (int i = 0; i < OutputCount && !reader2.EndOfStream ; ++i)
+            {
+                Console.WriteLine("Write {0} document...", i);
+                StreamWriter writer = new StreamWriter(Outputfolder + "\\out" + i + ".tsv");
+                for(int j = chunkSize * i; j < chunkSize*(i + 1) && !reader2.EndOfStream; j++)
+                {
+                    writer.WriteLine(reader2.ReadLine());
+                }
+                writer.Flush();
+            }
+            Console.Read();
+           
+        }
+    }
+}
